0d99ba1f7c4ef6b15f0ee53195da4cc0e0e4a0a9
kse201 <kse.201@gmail.com>
2017-02-28 11:56:40 +0900

    bugfix: modify rsync.sh

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -4,8 +4,6 @@
 #  # rsync.sh <src> <dest>
 #
 # Environment Parameters:
-#     log_dir: log file destination
-#     log_prefix: log filename prefix
 #     rsync_src: rsync file source url
 #     rsync_dest: rsync file destination
 
@@ -18,7 +16,8 @@ RSYNC_LOG=${log_dir}/${log_prefix}-`date "+%Y%m%d-%H%M%S"`.log
 
 echo "rsync -akv --delete ${rsync_src} ${rsync_dest}" > ${RSYNC_LOG}
 rsync -akvh --delete ${rsync_src} ${rsync_dest}  >> ${RSYNC_LOG} 2>&1
+rtn=$?
 
-echo `date "+%Y/%m/%d %H:%M:%S"`" finished, exit=0" >> ${RSYNC_LOG}
+echo `date "+%Y/%m/%d %H:%M:%S"`" finished, exit=${rtn}" >> ${RSYNC_LOG}
 
 exit 0
