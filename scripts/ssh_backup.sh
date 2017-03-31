88d0a63ee3cc57c9612920d8fad6d0397b5798b3
kse201 <kse.201@gmail.com>
2017-03-31 17:25:20 +0900

    bugfix: modify backup script

--- scripts/ssh_backup.sh
+++ scripts/ssh_backup.sh
@@ -2,5 +2,4 @@
 
 backup_files=".ssh"
 
-pushd /
-tar cJvf /vagrant/vagrant_ssh_$(date +%Y%m%d%M%S).tar.xz home/vagrant/${backup_files}
+tar cJvf "sshbackup_$(date +%Y%m%d%M%S).tar.xz" "${HOME}/${backup_files}"
