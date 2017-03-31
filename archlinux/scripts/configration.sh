4e781f93402ce1fdb9ba2e5d68f1722528819321
kse201 <kse.201@gmail.com>
2017-03-31 09:55:16 +0900

    feature: tweak archlinux configration

--- archlinux/scripts/configration.sh
+++ archlinux/scripts/configration.sh
@@ -1,17 +1,18 @@
-arch-chroot /mnt grub-install --force --recheck /dev/sda
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+chroot='arch-chroot /mnt'
+$chroot grub-install --force --recheck /dev/sda
+$chroot grub-mkconfig -o /boot/grub/grub.cfg
 genfstab -p /mnt >> /mnt/etc/fstab
 
 # basic configuration
-echo 'KEYMAP=jp106' > /mnt/etc/vconsole.conf
+$chroot loadkeys jp106
 
-sed -i \
+$chroot sed -i \
     -e 's/#en_US.UTF-8/en_US.UTF-8/' \
     -e 's/#ja_JP.UTF-8/ja_JP.UTF-8/' \
-    /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
-arch-chroot /mnt timedatectl set-timezone Asia/Tokyo
-arch-chroot /mnt hostnamectl set-hostname 'archlinux.localdomain'
+    /etc/locale.gen
+$chroot locale-gen
+$chroot timedatectl set-timezone Asia/Tokyo
+$chroot hostnamectl set-hostname 'archlinux.localdomain'
 
-arch-chroot /mnt systemctl enable dhcpcd
-arch-chroot /mnt systemctl enable sshd
+$chroot systemctl enable dhcpcd
+$chroot systemctl enable sshd
