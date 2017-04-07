4965b08341305faa6dab96076a3389791846e002
kse201 <kse.201@gmail.com>
2017-04-07 11:34:58 +0900

    feature: add script for running ansible playbook

---
+++ scripts/local-deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+target='localhost'
+inventory='inventory.ini'
+config='ansible.cfg'
+playbook='site.yml'
+
+cat << ... > ${inventory}
+[defaults]
+${target} ansible_connection=local
+...
+
+
+cat << ... > "${config}"
+[defaults]
+inventory=${inventory}
+roles_path=~/.ghq/github.com/kse201
+retry_files_enabled=False
+...
+
+cat << ... > "${playbook}"
+---
+- hosts: ${target}
+  roles:
+    - role: ansible-base
+  become: false
+...
+
+trap 'rm ${inventory} ${config} ${playbook}' 2
+
+ansible-playbook \
+    ./"${playbook}" "$@"
+
