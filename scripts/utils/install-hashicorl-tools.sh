5cbbd74b948ab8e8204ecc51d2f4a3020369bff8
kse201 <kse.201@gmail.com>
2017-06-08 11:23:06 +0900

    feaure: add scripts for install hashicorp tools!

---
+++ scripts/utils/install-hashicorl-tools.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -eu
+
+tmpdir=$(mktemp -d)
+atexit() {
+      [[ -n $tmpdir ]] && rm -rf "$tmpdir"
+  }
+  trap atexit EXIT
+trap 'trap - EXIT; atexit; exit -1' SIGHUP SIGINT SIGTERM
+
+os="linux"
+arch="amd64"
+
+for i in \
+    terraform,0.9.8 \
+    packer,1.0.0 \
+    nomad,0.5.6 \
+    consul,0.8.3 \
+    serf,0.8.1 \
+    vault,0.7.3 \
+    ;  do
+
+    IFS="," ; set -- $i
+    tool=$1
+    version=$2
+    curl -L -o "${tmpdir}/${tool}.zip" "https://releases.hashicorp.com/${tool}/${version}/${tool}_${version}_${os}_${arch}.zip"
+    unzip "${tmpdir}/${tool}.zip" -d "${tmpdir}"
+    sudo mv "${tmpdir}/${tool}" /usr/local/bin
+done
+
+rm -rf "${tmpdir}"
