f76f33faf45717e4e7d33e983c2829bc4d9c6fc5
kse201 <kse.201@gmail.com>
2017-07-31 15:26:01 +0900

    cosmetic: delivery.sh

--- scripts/utils/delivery.sh
+++ scripts/utils/delivery.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 usage() {
     cat <<...
@@ -18,8 +18,18 @@ archive=$(basename $PWD)
 target=$1
 dest_dir=$2
 
+echo "Step (1/3): Packaging"
 pushd ../
 tar czf ${archive}.tgz ${archive}
+
+echo "Step (2/3): Deploy"
 ssh ${target} mkdir -p ${dest_dir}
 scp ${archive}.tgz ${target}:${dest_dir}/
+
 ssh ${target} tar xz -C ${dest_dir} -f ${dest_dir}/${archive}.tgz
+
+echo "Step (3/3): Clean-up"
+ssh ${target} rm ${dest_dir}/${archive}.tgz
+rm ${archive}.tgz
+
+echo "All Steps Finished!"
