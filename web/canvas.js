ac92b079c8d1958a02918c3c7aa43185124023d7
kse201 <kse.201@gmail.com>
2017-08-03 19:01:17 +0900

    feature: learn js/canvas(wip)

---
+++ web/canvas.js
@@ -0,0 +1,6 @@
+function draw() {
+  var canvas = document.querySelector('#canvas');
+  var context = canvas.getContext('2d');
+  context.fillStyle = 'blue';
+  context.fillRect(100,100,100,100);
+}
