7bb9a039a8071f68504513cd3beaab572b881b78
kse201 <kse.201@gmail.com>
2018-11-14 15:56:15 +0900

    foo

--- kotlin/hello.kt
+++ kotlin/hello.kt
@@ -1,9 +1,22 @@
+class Person (var name: String = "default name", var age: Int = 0){
+
+    fun show () {
+        println("私は${this.name}です。${this.age}歳です。")
+    }
+}
 class Greeter(val name : String) {
-  fun greet() {
-    println("Hello, $name");
-  }
+    fun greet() {
+        println("Hello, $name");
+    }
 }
 
 fun main(args : Array<String>) {
-  Greeter(args[0]).greet()
+    /* Greeter(args[0]).greet() */
+    Greeter("foo").greet()
+
+    var foo = Person("foo", 20)
+    foo.show()
+
+    val bar = Person(age = 10)
+    bar.show()
 }
---
+++ kotlin/lib/package.kt
@@ -0,0 +1,9 @@
+package to.msn.wings.kotli.lib
+
+class Article() {
+
+}
+
+fun getTriangleArea(width: Double, height: Double) : Double {
+    return width * height /2
+}
