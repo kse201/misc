3e35a3bfb87c3a7a45a30b7d28a33b9201e6bdc3
kse201 <kse.201@gmail.com>
2019-12-27 14:14:22 +0900

    add golang util script

---
+++ scripts/utils/install_golang_utils.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+go get -u github.com/motemen/gore/cmd/gore
+go get -u github.com/nsf/gocode
+go get -u github.com/k0kubun/pp
+go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/lint/golint
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/gure
